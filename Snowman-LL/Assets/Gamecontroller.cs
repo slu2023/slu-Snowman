52d3e9f91f860de38be03ac6a991af1a62404241
slu2023 <slu2023@chadwickschool.org>
2021-12-02 12:20:36 -0800

    v1

--- Snowman-LL/Assets/Gamecontroller.cs
+++ Snowman-LL/Assets/Gamecontroller.cs
@@ -6,14 +6,18 @@ using WordGuesser;
 public class Gamecontroller : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Text HiddenWord;
+    public UnityEngine.UI.Text GuessAletter;
 
     public UnityEngine.UI.Button StartButton;
 
     public GameObject StartScreen;
     public GameObject PlayScreen;
     private WordGame guessingGame;
-
+    public UnityEngine.UI.Text Title;
     public UnityEngine.UI.InputField PlayerGuess;
+
+    public GameObject GameOverScreen;
 
 
     public void StartGame()
@@ -24,30 +28,47 @@ public class Gamecontroller : MonoBehaviour
 
         this.PlayScreen.gameObject.SetActive(true);
         this.StartScreen.gameObject.SetActive(false);
+        this.GameOverScreen.SetActive(false);
+
     }
 
     public void OpenStartScreen()
     {
-        this.Message.text = this.Message.text = "hello!!!!!!";
+        this.Message.text = this.Message.text = "Hi!!";
         this.PlayScreen.SetActive(false);
         this.StartScreen.SetActive(true);
+        this.GameOverScreen.SetActive(false);
+
     }
 
     public void Start()
     {
-        this.Message.text = this.Message.text = "hello!!!!!!";
+        this.Message.text = this.Message.text = "Welcome to Snowman";
         this.PlayScreen.SetActive(false);
+        this.GameOverScreen.SetActive(false);
+
         this.StartScreen.SetActive(true);
         this.guessingGame = new WordGame("banana", 5);
-        Debug.Log(this.guessingGame.GetWord());
+        this.HiddenWord.text = this.guessingGame.GetWord();
         Debug.Log(this.guessingGame.GetFullWord());
     }
 
     public void SubmitGuess()
     {
-        Debug.Log(this.guessingGame.CheckGuess(this.PlayerGuess.text));
+        this.GuessAletter.text = this.guessingGame.CheckGuess(this.PlayerGuess.text);
+        this.HiddenWord.text = this.guessingGame.GetWord();
+
+        if (this.guessingGame.IsGameOver())
+        {
+            this.Message.text = this.Message.text = "You lose! Now you're like this dude..";
+            this.PlayScreen.SetActive(false);
+            this.StartScreen.SetActive(false);
+            this.GameOverScreen.SetActive(true);
+
+        }
+
     }
 
-
+    // public void SubmitGuess
 }
 
